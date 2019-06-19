461e86ae390ca104a5cace01611485689a8bedb1
Vishal Lohkna <vishal.lohkna@bridgeimpact.org>
2019-06-19 12:35:37 +0530

    header ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarSearchComponent } from './car-search/car-search.component';
+
+// Routes
+const routes: Routes = [
+  { path: '', redirectTo: 'car-search', pathMatch: 'full' },
+  { path: 'car-search', component: CarSearchComponent},
+  { path: 'car-list', component: CarListComponent}
+];
 
 @NgModule({
   declarations: [
@@ -14,9 +23,12 @@ import { CarSearchComponent } from './car-search/car-search.component';
     CarSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
